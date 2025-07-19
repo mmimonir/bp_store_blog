0584c8f95ccbfb41d90a093bb3b0b8c6064e9237
Md. Monirul Islam <mmimonir.ctg@gmail.com>
2025-07-19 22:32:49 +0600

    Update footer logo image source to use storage path

--- resources/views/frontend/layouts/footer.blade.php
+++ resources/views/frontend/layouts/footer.blade.php
@@ -8,7 +8,8 @@
                     <!-- Single Widget -->
                     <div class="single-footer about">
                         <div class="logo">
-                            <a href="index.html"><img src="{{ asset('backend/img/logo2.png') }}" alt="#"></a>
+                            <a href="index.html"><img src="{{ asset('/storage/photos/31/fav_icon.png') }}"
+                                    alt="#"></a>
                         </div>
                         @php
                             $settings = DB::table('settings')->get();
