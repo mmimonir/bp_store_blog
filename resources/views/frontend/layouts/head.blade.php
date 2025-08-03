b28ecc7224106d078be8d551291137cfc9687754
Md. Monirul Islam <mmimonir.ctg@gmail.com>
2025-08-03 19:48:12 +0600

    Refactor small banner section and add featured products carousel for enhanced layout

--- resources/views/frontend/layouts/head.blade.php
+++ resources/views/frontend/layouts/head.blade.php
@@ -8,7 +8,6 @@
 <link
     href="https://fonts.googleapis.com/css?family=Poppins:200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i&display=swap"
     rel="stylesheet">
-
 <!-- StyleSheet -->
 <link rel="manifest" href="/manifest.json">
 <!-- Bootstrap -->
