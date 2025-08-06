36357267fb70c39e96c67ede13b5f69a06fe1bff
Md. Monirul Islam <mmimonir.ctg@gmail.com>
2025-08-06 13:05:51 +0600

    Add Tawk.to script for live chat support

--- resources/views/frontend/layouts/master.blade.php
+++ resources/views/frontend/layouts/master.blade.php
@@ -27,7 +27,21 @@
     @yield('main-content')
 
     @include('frontend.layouts.footer')
-
+    <!--Start of Tawk.to Script-->
+    <script type="text/javascript">
+        var Tawk_API = Tawk_API || {},
+            Tawk_LoadStart = new Date();
+        (function() {
+            var s1 = document.createElement("script"),
+                s0 = document.getElementsByTagName("script")[0];
+            s1.async = true;
+            s1.src = 'https://embed.tawk.to/6892fe500244bc1926d0f49a/1j1v32rlj';
+            s1.charset = 'UTF-8';
+            s1.setAttribute('crossorigin', '*');
+            s0.parentNode.insertBefore(s1, s0);
+        })();
+    </script>
+    <!--End of Tawk.to Script-->
 </body>
 
 </html>
